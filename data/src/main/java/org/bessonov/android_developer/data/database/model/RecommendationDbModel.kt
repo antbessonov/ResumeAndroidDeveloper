794808a02c6dad13b30cad9e19945019de3ae4d7
Anton Bessonov <anton@bessonov.org>
2023-06-19 18:27:32 +0700

    Создание RecommendationDbModel.kt

---
+++ data/src/main/java/org/bessonov/android_developer/data/database/model/RecommendationDbModel.kt
@@ -0,0 +1,19 @@
+package org.bessonov.android_developer.data.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recommendations")
+data class RecommendationDbModel(
+    val text: String,
+    @PrimaryKey
+    @ColumnInfo(name = "name_recommender")
+    val nameRecommender: String,
+    @ColumnInfo(name = "photo_recommender")
+    val photoRecommender: String,
+    @ColumnInfo(name = "job_title_recommender")
+    val jobTitleRecommender: String,
+    @ColumnInfo(name = "name_company_recommender")
+    val nameCompanyRecommender: String
+)
