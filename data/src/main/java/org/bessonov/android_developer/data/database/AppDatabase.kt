8ed5dc2f8ebd9826f53ce8caa87ef0f4d615c745
Anton Bessonov <anton@bessonov.org>
2023-06-23 15:41:11 +0700

    Редактирование AppDatabase.kt

--- data/src/main/java/org/bessonov/android_developer/data/database/AppDatabase.kt
+++ data/src/main/java/org/bessonov/android_developer/data/database/AppDatabase.kt
@@ -3,12 +3,18 @@ package org.bessonov.android_developer.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import org.bessonov.android_developer.data.database.model.RecommendationDbModel
+import org.bessonov.android_developer.data.database.model.SkillDbModel
+import org.bessonov.android_developer.data.database.model.SkillGroupDbModel
 
 @Database(
-    entities = [RecommendationDbModel::class],
+    entities = [SkillGroupDbModel::class, SkillDbModel::class, RecommendationDbModel::class],
     version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun skillGroupDao(): SkillGroupDao
+
+    abstract fun skillDao(): SkillDao
 
     abstract fun recommendationDao(): RecommendationDao
 }
