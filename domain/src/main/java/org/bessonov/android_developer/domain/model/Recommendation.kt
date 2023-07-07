dec9eba7853fedde2d9b085f8f1d7efa786f90be
Anton Bessonov <anton@bessonov.org>
2023-07-07 18:32:54 +0700

    Добавление поля recommender в Recommendation.kt

--- domain/src/main/java/org/bessonov/android_developer/domain/model/Recommendation.kt
+++ domain/src/main/java/org/bessonov/android_developer/domain/model/Recommendation.kt
@@ -2,8 +2,5 @@ package org.bessonov.android_developer.domain.model
 
 data class Recommendation(
     val text: String,
-    val nameRecommender: String,
-    val photoRecommender: Int,
-    val jobTitleRecommender: String,
-    val nameCompanyRecommender: String
+    val recommender: Recommender,
 )
