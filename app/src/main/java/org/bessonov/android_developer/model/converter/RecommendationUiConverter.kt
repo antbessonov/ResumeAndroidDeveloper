b61b6dc2b2d8b2996274aa1218ec80e46b867ac6
Anton Bessonov <anton@bessonov.org>
2023-07-07 18:44:39 +0700

    Редактирование convertEntityToUiModel() в RecommendationUiConverter.kt

--- app/src/main/java/org/bessonov/android_developer/model/converter/RecommendationUiConverter.kt
+++ app/src/main/java/org/bessonov/android_developer/model/converter/RecommendationUiConverter.kt
@@ -2,10 +2,11 @@ package org.bessonov.android_developer.model.converter
 
 import org.bessonov.android_developer.domain.model.Recommendation
 import org.bessonov.android_developer.model.RecommendationUi
-import org.bessonov.android_developer.model.StoredPhotoRecommenderDrawable
 import javax.inject.Inject
 
-class RecommendationUiConverter @Inject constructor() {
+class RecommendationUiConverter @Inject constructor(
+    private val recommenderUiConverter: RecommenderUiConverter
+) {
 
     fun convertEntityListToUiModelList(entityList: List<Recommendation>): List<RecommendationUi> {
         return entityList.map { entity ->
@@ -16,10 +17,7 @@ class RecommendationUiConverter @Inject constructor() {
     private fun convertEntityToUiModel(entity: Recommendation): RecommendationUi {
         return RecommendationUi(
             text = entity.text,
-            nameRecommender = entity.nameRecommender,
-            photoRecommenderResId = StoredPhotoRecommenderDrawable.asRes(storedPhoto = entity.photoRecommender),
-            jobTitleRecommender = entity.jobTitleRecommender,
-            nameCompanyRecommender = entity.nameCompanyRecommender
+            recommender = recommenderUiConverter.convertEntityToUiModel(entity = entity.recommender)
         )
     }
 }
