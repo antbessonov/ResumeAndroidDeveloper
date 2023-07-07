edcb8fc4fde0d9c60266da8835a0d80f6865eddc
Anton Bessonov <anton@bessonov.org>
2023-07-07 18:41:54 +0700

    Добавление поля recommender в RecommendationUi.kt

--- app/src/main/java/org/bessonov/android_developer/model/RecommendationUi.kt
+++ app/src/main/java/org/bessonov/android_developer/model/RecommendationUi.kt
@@ -1,11 +1,6 @@
 package org.bessonov.android_developer.model
 
-import androidx.annotation.DrawableRes
-
 data class RecommendationUi(
     val text: String,
-    val nameRecommender: String,
-    @DrawableRes val photoRecommenderResId: Int,
-    val jobTitleRecommender: String,
-    val nameCompanyRecommender: String
+    val recommender: RecommenderUi
 )
