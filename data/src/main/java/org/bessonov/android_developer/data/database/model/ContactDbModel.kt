890aa6a0368601b4ac078834c8da6f91b7b74fd4
Anton Bessonov <anton@bessonov.org>
2023-07-08 16:12:26 +0700

    Создание ContactDbModel.kt

---
+++ data/src/main/java/org/bessonov/android_developer/data/database/model/ContactDbModel.kt
@@ -0,0 +1,21 @@
+package org.bessonov.android_developer.data.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact.xml")
+data class ContactDbModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "telegram_id")
+    val telegramId: String,
+    val mail: String,
+    val tel: String,
+    @ColumnInfo(name = "linkedin_id")
+    val linkedinId: String,
+    @ColumnInfo(name = "github_id")
+    val githubId: String,
+    val latitude: Double,
+    val longitude: Double
+)
