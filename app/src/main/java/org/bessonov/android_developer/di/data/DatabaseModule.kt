e6817f13868b48cdbdb3a7acd3ec8a1151e6089a
Anton Bessonov <anton@bessonov.org>
2023-07-08 16:27:25 +0700

    Редактирование DatabaseModule.kt

--- app/src/main/java/org/bessonov/android_developer/di/data/DatabaseModule.kt
+++ app/src/main/java/org/bessonov/android_developer/di/data/DatabaseModule.kt
@@ -6,12 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.bessonov.android_developer.data.database.AppDatabase
-import org.bessonov.android_developer.data.database.RecommendationDao
-import org.bessonov.android_developer.data.database.SkillDao
-import org.bessonov.android_developer.data.database.SkillGroupDao
+import org.bessonov.android_developer.data.database.*
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -27,20 +23,44 @@ object DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideSkillGroupDao(appDatabase: AppDatabase): SkillGroupDao {
-        return appDatabase.skillGroupDao()
+    fun provideHardSkillGroupDao(appDatabase: AppDatabase): HardSkillGroupDao {
+        return appDatabase.hardSkillGroupDao()
     }
 
     @Provides
     @Singleton
-    fun provideSkillDao(appDatabase: AppDatabase): SkillDao {
-        return appDatabase.skillDao()
+    fun provideHardSkillDao(appDatabase: AppDatabase): HardSkillDao {
+        return appDatabase.hardSkillDao()
     }
 
     @Provides
     @Singleton
     fun provideRecommendationDao(appDatabase: AppDatabase): RecommendationDao {
         return appDatabase.recommendationDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubProjectDao(appDatabase: AppDatabase): GitHubProjectDao {
+        return appDatabase.gitHubProjectDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubProjectHardSkillDao(appDatabase: AppDatabase): GitHubProjectHardSkillDao {
+        return appDatabase.gitHubProjectHardSkillDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideEducationDao(appDatabase: AppDatabase): EducationDao {
+        return appDatabase.educationDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideContactDao(appDatabase: AppDatabase): ContactDao {
+        return appDatabase.contactDao()
     }
 
     private const val DB_NAME = "android_developer.db"
