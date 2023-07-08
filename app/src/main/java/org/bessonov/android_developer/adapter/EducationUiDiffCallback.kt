c5468a2d3f4ce14a14dbb80a3e89b065677f74f6
Anton Bessonov <anton@bessonov.org>
2023-07-08 15:45:28 +0700

    Создание EducationUiDiffCallback.kt

---
+++ app/src/main/java/org/bessonov/android_developer/adapter/EducationUiDiffCallback.kt
@@ -0,0 +1,15 @@
+package org.bessonov.android_developer.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import org.bessonov.android_developer.model.EducationUi
+
+object EducationUiDiffCallback : DiffUtil.ItemCallback<EducationUi>() {
+
+    override fun areItemsTheSame(oldItem: EducationUi, newItem: EducationUi): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: EducationUi, newItem: EducationUi): Boolean {
+        return oldItem == newItem
+    }
+}
