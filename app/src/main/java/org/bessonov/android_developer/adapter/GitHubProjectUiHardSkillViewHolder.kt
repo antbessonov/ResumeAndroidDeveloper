6862e8777a061e773f01568a67f5ab57c47d34f6
Anton Bessonov <anton@bessonov.org>
2023-07-08 14:23:59 +0700

    Создание GitHubProjectUiHardSkillViewHolder.kt

---
+++ app/src/main/java/org/bessonov/android_developer/adapter/GitHubProjectUiHardSkillViewHolder.kt
@@ -0,0 +1,17 @@
+package org.bessonov.android_developer.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.bessonov.android_developer.R
+import org.bessonov.android_developer.databinding.GithubProjectHardSkillItemBinding
+
+class GitHubProjectUiHardSkillViewHolder(val parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(
+        R.layout.github_project_hard_skill_item,
+        parent,
+        false
+    )
+) {
+    val binding = GithubProjectHardSkillItemBinding.bind(itemView)
+}
