65ec5e20a0c2784acae53c0e6751e9ff969ee6ae
Anton Bessonov <anton@bessonov.org>
2023-07-08 13:27:17 +0700

    Создание GitHubProjectDbModel.kt

---
+++ data/src/main/java/org/bessonov/android_developer/data/database/model/GitHubProjectDbModel.kt
@@ -0,0 +1,15 @@
+package org.bessonov.android_developer.data.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "github_projects")
+class GitHubProjectDbModel(
+    @PrimaryKey
+    @ColumnInfo(name = "name_github_project")
+    val name: String,
+    val description: String,
+    @ColumnInfo(name = "date_creation")
+    val dateCreation: String
+)
