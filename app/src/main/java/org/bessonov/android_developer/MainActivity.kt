56754ae447fc9a8726bd2233eff0dc9509cf555c
Anton Bessonov <anton@bessonov.org>
2023-07-08 12:57:32 +0700

    Добавление BottomNavigationView

--- app/src/main/java/org/bessonov/android_developer/MainActivity.kt
+++ app/src/main/java/org/bessonov/android_developer/MainActivity.kt
@@ -2,10 +2,23 @@ package org.bessonov.android_developer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.bessonov.android_developer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding: ActivityMainBinding by viewBinding(R.id.main_container)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = binding.mainFragmentContainer
+            .getFragment<NavHostFragment>()
+            .navController
+        navView.setupWithNavController(navController)
     }
 }
