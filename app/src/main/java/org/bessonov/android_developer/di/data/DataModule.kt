1a1140c4db99e0b52cba45139e531bafca6036a8
Anton Bessonov <anton@bessonov.org>
2023-07-08 16:27:43 +0700

    Редактирование DataModule.kt

--- app/src/main/java/org/bessonov/android_developer/di/data/DataModule.kt
+++ app/src/main/java/org/bessonov/android_developer/di/data/DataModule.kt
@@ -4,16 +4,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.bessonov.android_developer.data.database.RecommendationDao
-import org.bessonov.android_developer.data.database.SkillDao
-import org.bessonov.android_developer.data.database.SkillGroupDao
-import org.bessonov.android_developer.data.mapper.RecommendationMapper
-import org.bessonov.android_developer.data.mapper.SkillGroupMapper
-import org.bessonov.android_developer.data.mapper.SkillMapper
-import org.bessonov.android_developer.data.repository.RecommendationRepositoryImpl
-import org.bessonov.android_developer.data.repository.SkillGroupRepositoryImpl
+import org.bessonov.android_developer.data.database.*
+import org.bessonov.android_developer.data.mapper.*
+import org.bessonov.android_developer.data.network.APIService
+import org.bessonov.android_developer.data.repository.*
+import org.bessonov.android_developer.domain.repository.ContactRepository
+import org.bessonov.android_developer.domain.repository.GitHubProjectRepository
+import org.bessonov.android_developer.domain.repository.HardSkillGroupRepository
 import org.bessonov.android_developer.domain.repository.RecommendationRepository
-import org.bessonov.android_developer.domain.repository.SkillGroupRepository
 import javax.inject.Singleton
 
 @Module
@@ -22,15 +20,15 @@ object DataModule {
 
     @Provides
     @Singleton
-    fun provideSkillGroupRepository(
-        skillGroupDao: SkillGroupDao,
-        skillDao: SkillDao,
-        skillGroupMapper: SkillGroupMapper
-    ): SkillGroupRepository {
-        return SkillGroupRepositoryImpl(
-            skillGroupDao = skillGroupDao,
-            skillDao = skillDao,
-            skillGroupMapper = skillGroupMapper
+    fun provideHardSkillGroupRepository(
+        hardSkillGroupDao: HardSkillGroupDao,
+        hardSkillDao: HardSkillDao,
+        hardSkillGroupMapper: HardSkillGroupMapper
+    ): HardSkillGroupRepository {
+        return HardSkillGroupRepositoryImpl(
+            hardSkillGroupDao = hardSkillGroupDao,
+            hardSkillDao = hardSkillDao,
+            hardSkillGroupMapper = hardSkillGroupMapper
         )
     }
 
@@ -48,19 +46,104 @@ object DataModule {
 
     @Provides
     @Singleton
-    fun provideSkillGroupMapper(skillMapper: SkillMapper): SkillGroupMapper {
-        return SkillGroupMapper(skillMapper = skillMapper)
+    fun provideGitHubProjectRepository(
+        apiService: APIService,
+        loadingResultMapper: LoadingResultMapper,
+        gitHubProjectDao: GitHubProjectDao,
+        gitHubProjectMapper: GitHubProjectMapper,
+        gitHubProjectHardSkillDao: GitHubProjectHardSkillDao,
+        gitHubProjectHardSkillMapper: GitHubProjectHardSkillMapper,
+        hardSkillDao: HardSkillDao,
+        hardSkillGroupDao: HardSkillGroupDao
+    ): GitHubProjectRepository {
+        return GitHubProjectRepositoryImpl(
+            apiService = apiService,
+            loadingResultMapper = loadingResultMapper,
+            gitHubProjectDao = gitHubProjectDao,
+            gitHubProjectMapper = gitHubProjectMapper,
+            gitHubProjectHardSkillDao = gitHubProjectHardSkillDao,
+            gitHubProjectHardSkillMapper = gitHubProjectHardSkillMapper,
+            hardSkillDao = hardSkillDao,
+            hardSkillGroupDao = hardSkillGroupDao
+        )
     }
 
     @Provides
     @Singleton
-    fun provideSkillMapper(): SkillMapper {
-        return SkillMapper()
+    fun provideEducationRepositoryImpl(
+        educationDao: EducationDao,
+        educationMapper: EducationMapper
+    ): EducationRepositoryImpl {
+        return EducationRepositoryImpl(
+            educationDao = educationDao,
+            educationMapper = educationMapper
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provide(
+        contactDao: ContactDao,
+        contactMapper: ContactMapper
+    ): ContactRepository {
+        return ContactRepositoryImpl(contactDao = contactDao, contactMapper = contactMapper)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHardSkillGroupMapper(
+        skillMapper: HardSkillMapper
+    ): HardSkillGroupMapper {
+        return HardSkillGroupMapper(hardSkillMapper = skillMapper)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHardSkillMapper(): HardSkillMapper {
+        return HardSkillMapper()
     }
 
     @Provides
     @Singleton
     fun provideRecommendationMapper(): RecommendationMapper {
         return RecommendationMapper()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoadingResultMapper(): LoadingResultMapper {
+        return LoadingResultMapper()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubProjectHardSkillMapper(
+        hardSkillMapper: HardSkillMapper,
+        hardSkillGroupMapper: HardSkillGroupMapper
+    ): GitHubProjectHardSkillMapper {
+        return GitHubProjectHardSkillMapper(
+            hardSkillMapper = hardSkillMapper,
+            hardSkillGroupMapper = hardSkillGroupMapper
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubProjectMapper(
+        gitHubProjectHardSkillMapper: GitHubProjectHardSkillMapper
+    ): GitHubProjectMapper {
+        return GitHubProjectMapper(gitHubProjectHardSkillMapper = gitHubProjectHardSkillMapper)
+    }
+
+    @Provides
+    @Singleton
+    fun provideEducationMapper(): EducationMapper {
+        return EducationMapper()
+    }
+
+    @Provides
+    @Singleton
+    fun provideContactMapper(): ContactMapper {
+        return ContactMapper()
     }
 }
