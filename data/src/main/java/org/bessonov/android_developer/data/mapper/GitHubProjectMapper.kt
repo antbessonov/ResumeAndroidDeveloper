492228a4c0f96ca4192c4f7ec754745d322d6759
Anton Bessonov <anton@bessonov.org>
2023-07-08 14:00:01 +0700

    Создание GitHubProjectMapper.kt

---
+++ data/src/main/java/org/bessonov/android_developer/data/mapper/GitHubProjectMapper.kt
@@ -0,0 +1,64 @@
+package org.bessonov.android_developer.data.mapper
+
+import org.bessonov.android_developer.data.database.model.GitHubProjectDbModel
+import org.bessonov.android_developer.data.database.model.GitHubProjectHardSkillDbModel
+import org.bessonov.android_developer.data.database.model.HardSkillDbModel
+import org.bessonov.android_developer.data.database.model.HardSkillGroupDbModel
+import org.bessonov.android_developer.data.network.model.GitHubProjectDto
+import org.bessonov.android_developer.domain.model.GitHubProject
+
+class GitHubProjectMapper(
+    private val gitHubProjectHardSkillMapper: GitHubProjectHardSkillMapper
+) {
+
+    fun mapDbModelListToEntityList(
+        gitHubProjectDbModelList: List<GitHubProjectDbModel>,
+        gitHubProjectHardSkillDbModelList: List<GitHubProjectHardSkillDbModel>,
+        hardSkillDbModelList: List<HardSkillDbModel>,
+        hardSkillGroupDbModelList: List<HardSkillGroupDbModel>
+    ): List<GitHubProject> {
+        return gitHubProjectDbModelList
+            .map { dbModel ->
+                mapDbModelToEntity(
+                    dbModel = dbModel,
+                    gitHubProjectHardSkillDbModelList = gitHubProjectHardSkillDbModelList,
+                    hardSkillDbModelList = hardSkillDbModelList,
+                    hardSkillGroupDbModelList = hardSkillGroupDbModelList
+                )
+            }
+    }
+
+    fun mapDtoListToDbModelList(dtoList: List<GitHubProjectDto>): List<GitHubProjectDbModel> {
+        return dtoList
+            .map { dto ->
+                mapDtoToDbModel(dto = dto)
+            }
+    }
+
+    private fun mapDbModelToEntity(
+        dbModel: GitHubProjectDbModel,
+        gitHubProjectHardSkillDbModelList: List<GitHubProjectHardSkillDbModel>,
+        hardSkillDbModelList: List<HardSkillDbModel>,
+        hardSkillGroupDbModelList: List<HardSkillGroupDbModel>
+    ): GitHubProject {
+        return GitHubProject(
+            name = dbModel.name,
+            description = dbModel.description,
+            dateCreation = dbModel.dateCreation,
+            hardSkillGroupList = gitHubProjectHardSkillMapper.mapDbModelListToEntityList(
+                name = dbModel.name,
+                dbModelList = gitHubProjectHardSkillDbModelList,
+                hardSkillDbModelList = hardSkillDbModelList,
+                hardSkillGroupDbModelList = hardSkillGroupDbModelList
+            )
+        )
+    }
+
+    private fun mapDtoToDbModel(dto: GitHubProjectDto): GitHubProjectDbModel {
+        return GitHubProjectDbModel(
+            name = dto.name,
+            description = dto.description,
+            dateCreation = dto.dateCreation,
+        )
+    }
+}
